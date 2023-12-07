bb4b6ebdff30f0398a871a14121e1e98cca0f5c0
Albert Lionelle <albert.lionelle@colostate.edu>
2023-12-06 23:26:37 -0700

    updated with new timing function, revamped script

--- Module06/pascal.c
+++ Module06/pascal.c
@@ -1,21 +1,55 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <time.h>
+#include <stdint.h>
 
-#define MAX 50000
+#define MAX 5000
+#define PRINT_SINGLE 1
+#define PRINT_ALL 2
 
-typedef unsigned long long ull;
+typedef uint64_t ull;
 
 // don't forget to allocate space for your table (multi-demi arrayof MAX x MAX)
+static ull table[MAX][MAX];
 
-ull pascaldp(int n, int i)
+void printSingleRow(ull *row, int size)
 {
- // todo by student
-
- return 0; // just here to compile, delete
+    for (int i = 0; i <= size; i++)
+    {
+        printf("%llu ", row[i]);
+    }
+    printf("\n");
 }
 
-ull pascalr(int n, int i)
+ull pascaldp(int n, int i, int *ops)
+{
+    if (n == i || i == 0)
+    {
+        return 1;
+    }
+    else if (table[n][i] > 0)
+    {
+        return table[n][i];
+    }
+    else
+    {
+        (*ops)++;
+        table[n][i] = pascaldp(n - 1, i, ops) + pascaldp(n - 1, i - 1, ops);
+        return table[n][i]; // pascal(n - 1, i) + pascal(n - 1, i - 1);
+    }
+}
+
+ull* pascaldp_full(int n, int *ops)
+{
+    ull *row = malloc((sizeof(ull)) * (n+1));
+    for (int i = 0; i <= n; i++)
+    {
+        row[i] = pascaldp(n, i, ops);
+    }
+    return row;
+}
+ull pascalr(int n, int i, int *ops)
 {
     if (n == i || i == 0)
     {
@@ -23,78 +57,83 @@ ull pascalr(int n, int i)
     }
     else
     {
-        return pascalr(n - 1, i) + pascalr(n - 1, i - 1);
+        (*ops)++;
+        return pascalr(n - 1, i, ops) + pascalr(n - 1, i - 1, ops);
     }
 }
-ull *recursion_helper(int n, bool print, int version) {
-    ull * row = malloc((sizeof(ull)) * n);
-    if(print) {
-       for (int i = 0; i < n; i++)
-       {
-            ull tmp = 1;
-            for (int j = 0; j <= i; j++)
-            {
-                if(version == 2) tmp = pascaldp(i, j);
-                else if(version == 1) {
-                    tmp = pascalr(i, j);
-                }
-                 if(i+1 == n) row[j] = tmp;
-                 printf("%lu ", tmp);
-            }
-            printf("\n");
-        }
-    }else {
-        n--; // for ob1 error
-        for(int i = 0; i <= n; i++) {
-            ull tmp = 1;
-            if(version == 2) tmp = pascaldp(n, i);
-            else tmp = pascalr(n, i);
-            row[i] = tmp;
-        }
+
+ull* pascalr_full(int n, int *ops)
+{
+    ull *row = malloc((sizeof(ull)) * (n+1));
+    for (int i = 0; i <= n; i++)
+    {
+        row[i] = pascalr(n, i, ops);
     }
     return row;
 }
 
-void printSingleRow(ull* row, int size) {
-    for(int i = 0; i < size; i++) {
-        printf("%lu ", row[i]);
-    }
-    printf("\n");
-}
 
-ull *getPascalTriangle(int n, bool print, int version)
+ull *pascal_iterative(int n, int *ops)
 {
-
-    if(version > 0) {
-        return recursion_helper(n, print, version);
-    }
-   // implied else for iterative version
-     ull** triangle = (ull**) malloc((n+1) * sizeof(ull*));
-    for (int i = 0; i < n; i++)
+    ull **triangle = (ull **)malloc((n + 1) * sizeof(ull *));
+    for (int i = 0; i <= n; i++)
     {
-        triangle[i] = (ull*) malloc((i+1) * sizeof(ull));
+        triangle[i] = (ull *)malloc((i + 1) * sizeof(ull));
         triangle[i][0] = 1;
         for (int j = 1; j <= i; j++)
         {
-            if(i == j)  {
+            (*ops)++;
+            if (i == j)
+            {
                 triangle[i][j] = 1;
             }
-            else {
-                triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j];
+            else
+            {
+                triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
             }
         }
-        if (print) printSingleRow(triangle[i], i+1);
     }
-    //if(print) printf("\n");
+    // copy the last row into a new array to return
+    ull *row = malloc((sizeof(ull)) * (n+1));
+    for (int i = 0; i <= n; i++)
+    {
+        row[i] = triangle[n][i];
+    }
+    // free the triangle
+    for (int i = 0; i <= n; i++)
+    {
+        free(triangle[i]);
+    }
+    free(triangle);
 
-    return triangle[n-1];
+    // give back the single row
+    return row;
 }
+
+
+
+double time_function(ull* (*func)(int, int *), int n, int *ops, bool print)
+{
+    // Setup timers
+    struct timespec begin, end;
+    // Get the time before we start
+    clock_gettime(CLOCK_MONOTONIC, &begin);
+    ull* row = func(n, ops);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    if(print) {
+        printSingleRow(row, n);
+    }
+    return (end.tv_nsec - begin.tv_nsec) / 1000000000.0 +
+           (end.tv_sec - begin.tv_sec);
+}
+
+
 
 void help() {
     printf("./pascal.out N [Type] [Print Level]\n");
     printf("\tN is the number of rows in the pascal triangle, required.\n");
-    printf("\t[Type] is either 2 for dynamic programming version, 1 for recursive version, 0 for iterative version, defaults to 0.\n");
-    printf("\t[Print Level] is 0 for no print/speed compare only, 1 for print final row only, 2 for print all rows, defaults to 0.\n");
+    printf("\t[Type] is 4 for dp and iterative only, 3 for all three, 2 for dynamic programming version, 1 for recursive version, 0 for iterative version, defaults to 3.\n");
+    printf("\t[Print Level] leave blank for speed compare only, or print to print row\n");
 
 }
 
@@ -108,20 +147,61 @@ int main(int argc, char* argv[])
     }
 
     const int n = atoi(argv[1]);
-    int type = 0;
+    int type = 3;
     int print = 0;
     if (argc > 2) {
         type = atoi(argv[2]);
     }
     if(argc > 3) {
-        print = atoi(argv[3]);
+        print = true;
     }
 
-    bool print_it = false;
-    if (print > 1) {
-        print_it = true;
+    int ops;
+    double time;
+    switch (type)
+    {
+    case 0:
+        printf("iterative version\n");
+        ops = 0;
+        time = time_function(pascal_iterative, n, &ops, print);
+        printf("time: %f(%d)\n", time, ops);
+        break;
+    case 1:
+        printf("recursive version\n");
+        ops = 0;
+        time = time_function(pascalr_full, n, &ops, print);
+        printf("time: %f(%d)\n", time, ops);
+        break;
+    case 2:
+        printf("dynamic programming version\n");
+        ops = 0;
+        time = time_function(pascaldp_full, n, &ops, print);
+        printf("time: %f(%d)\n", time, ops);
+        break;
+    case 4:
+        ops = 0;
+        time = time_function(pascal_iterative, n, &ops, print);
+        printf("%f,%d,", time, ops);
+
+        ops = 0;
+        time = time_function(pascaldp_full, n, &ops, print);
+        printf("%f,%d,--", time, ops);
+        break;
+    default:
+        ops = 0;
+        time = time_function(pascal_iterative, n, &ops, print);
+        printf("%f,%d,", time, ops);
+
+        ops = 0;
+        time = time_function(pascaldp_full, n, &ops, print);
+        printf("%f,%d,", time, ops);
+
+        ops = 0;
+        time = time_function(pascalr_full, n, &ops, print);
+        printf("%f,%d", time, ops);
+
+        break;
     }
-    ull *row_at_n = getPascalTriangle(n, print_it, type);
-    if (print == 1) printSingleRow(row_at_n, n);
-    return 0;
+
+
 }
--- Module06/pascal.py
+++ Module06/pascal.py
@@ -12,19 +12,25 @@ python3 test_runner.py 30 > pascal_table.md
 """
 
 import subprocess
-import time 
 import sys
-import math
+import csv
 
-EXEC = ["./pascal.out"] 
-COMMON_ARG_FORMAT = "{n} {type} 0"
-FORMAT = "markdown"
+EXEC = ["./pascal.exe"]
+COMMON_ARG_FORMAT = "{n} {type}"
 TIMEOUT = 60
+OUT_DEFAULT = "pascal_run.csv"
+OUT_FILE_TIME = "timings_"
+OUT_FILE_OPS = "ops_"
+CSV_HEADER = "N,Iterative,Dynamic Programming,Recursive"
 
-LAST_RUN_TRACKER = [0.0, 0.0, 0.0]
 
-def run_single(n: int, typ: int) -> float:
-    """Run a single instance collecting the total execution time
+class RecursionTimeoutError(Exception):
+    pass
+
+
+def run_single(n: int, typ: int) -> dict:
+    """Run a single instance collecting the values into a dictionary
+    with two tuples, one for timings and one for operations
 
     Args:
         n (int): the row to generate
@@ -34,55 +40,60 @@ def run_single(n: int, typ: int) -> float:
         float: the time it took, or nan if TIMEOUT is reached first
     """
     args = COMMON_ARG_FORMAT.format(n=n, type=typ)
-    if math.isnan(LAST_RUN_TRACKER[typ]): return math.nan  # skip running if we are already timing out
     try:
-        start = time.time()
-        subprocess.run(EXEC + args.split(), timeout=TIMEOUT)
-        end = time.time()
-        result = end - start
-        
+        results = subprocess.run(
+            EXEC + args.split(), timeout=TIMEOUT, capture_output=True, text=True
+        )
     except subprocess.TimeoutExpired:
-        result = math.nan
-    LAST_RUN_TRACKER[typ] = result
-    return result
+        raise RecursionTimeoutError(f"Timeout of {TIMEOUT} seconds reached for {args}")
 
-def build_row(n:int) -> str:
-    """Builds a row to print to the screen either in csv format or markdown
+    if results.returncode != 0:
+        raise Exception(f"Error running {args}: {results.stderr}")
 
+    results_line = results.stdout.strip().split(",")
+    timings = []
+    operations = []
+    for i in range(0, len(results_line), 2):
+        timings.append(results_line[i])
+        operations.append(results_line[i + 1])
+
+    return {"timings": timings, "operations": operations}
+
+
+def save_to_csv(values: list, out_file: str):
+    """saves a list to a csv file
     Args:
-        n (int): the row to build in the triangle
-
-    Returns:
-        str: a markdown formatted or csv string of the result
+        results (list): the results to save
+        out_file (str): the base file name to write to
     """
-    results_lst = []
-    for t in range(0,3):
-        result = run_single(n, t)
-        results_lst.append("-" if math.isnan(result) else f"{result:.5f}")
-    iterative, recursive, dynamic_programming = results_lst 
-    if FORMAT == "markdown":
-        return f"| {n:<4} | {iterative.center(8, ' ')} | {recursive.center(8, ' ')} | {dynamic_programming.center(8, ' ')} |"
-    return f"{n},{iterative},{recursive},{dynamic_programming}"
+    with open(out_file, "w", newline='') as f:
+        csv_writer = csv.writer(f)
+        csv_writer.writerow(CSV_HEADER.split(","))
+        for i, row in enumerate(values):
+            row = [i+1] + row
+            csv_writer.writerow(row)
 
+def main(n, step=1, out_file=OUT_DEFAULT):
+    run_type = 3
+    results = {"timings": [], "operations": []}
+    for i in range(1, n + 1, step):
+        try:
+            result = run_single(i, run_type)
+            results["timings"].append(result["timings"])
+            results["operations"].append(result["operations"])
+        except RecursionTimeoutError as e:
+            run_type = 4
+            break
+        except Exception as e:
+            print(e, file=sys.stderr)
+            break
+    save_to_csv(results["operations"], OUT_FILE_OPS + out_file)
+    save_to_csv(results["timings"], OUT_FILE_TIME + out_file)
 
-
-def table_header() -> str:
-    """Returns a markdown table header for this data set"""
-    if FORMAT == "markdown":
-        return "| n | Iterative | Recursive | Dynamic Programming |\n" + \
-               "|--|:--:|:--:|:--:|"
-    return "n,Iterative,Recursive,Dynamic Programming"
-
-
-
-def main(n):
-    print(table_header())
-    for i in range(1,n+1):
-        print(build_row(i))
 
 ## note while using argv directly, there are better tools for this like pip click
 if __name__ == "__main__":
-    n = 30 if len(sys.argv) < 2 else int(sys.argv[1])   
+    n = 30 if len(sys.argv) < 2 else int(sys.argv[1])
     if len(sys.argv) == 3:
         FORMAT = "csv"
     main(n)
